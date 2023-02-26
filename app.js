b651dd28f9f0d941f563bb0119e1c21184b37af4
Abhishek Chamoli <104618483+AbhishekChamoliDeveloper@users.noreply.github.com>
2023-02-26 22:18:28 +0530

    MongoDB CRUD OPERATIONS

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const userRouter = require("./routers/userRouter");
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connect To Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,79 +1,61 @@
-const fs = require("fs");
-const path = require("path");
-
-// Global Filepath
-const filepath = path.join(__dirname, "../data", "users.json");
+const User = require("../models/userModel");
 
 // Getting All Users Informatins
 exports.getAllUsers = async (req, res) => {
-  const data = await fs.promises.readFile(filepath, "utf-8");
-  const users = JSON.parse(data);
-  res.status(200).json(users);
+  const users = await User.find();
+
+  res.status(200).json({
+    totalUser: users.length,
+    data: users,
+  });
 };
 
 // Creating New User
 exports.createUser = async (req, res) => {
-  const data = await fs.promises.readFile(filepath, "utf-8");
-  const users = JSON.parse(data);
+  const { name, email, password } = req.body;
 
-  const newUser = req.body;
-  users.push(newUser);
+  const newUser = await User.create({ name, email, password });
 
-  const userJson = JSON.stringify(users);
-  await fs.promises.writeFile(filepath, userJson, "utf-8");
-
-  res.status(200).json(users);
+  res.status(201).json({
+    message: "User has been Created",
+    data: newUser,
+  });
 };
 
 // Getting User By Id
 exports.getUserById = async (req, res) => {
   const id = req.params.id;
 
-  const data = await fs.promises.readFile(filepath, "utf-8");
-  const users = JSON.parse(data);
+  const user = await User.find({ _id: id });
 
-  const userIndex = users.findIndex((user) => user.id === parseInt(id));
-
-  if (userIndex === -1) {
-    return res.status(404).json({ message: "User not found" });
+  if (!user) {
+    res.status(404).json({ message: "User not founed" });
   }
 
-  res.status(200).json(users[userIndex]);
+  res.status(200).json({
+    data: user,
+  });
 };
 
 // Updating By Id
 exports.updateUserById = async (req, res) => {
   const id = req.params.id;
+  const body = req.body;
 
-  const data = await fs.promises.readFile(filepath, "utf-8");
-  const users = JSON.parse(data);
+  const updatedUser = await User.findByIdAndUpdate(id, body, {
+    new: true,
+  });
 
-  const userIndex = users.findIndex((user) => user.id === parseInt(id));
-
-  if (userIndex === -1) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  const updatedUser = { ...users[userIndex], ...req.body };
-  users[userIndex] = updatedUser;
-
-  const userJson = JSON.stringify(users);
-  await fs.promises.writeFile(filepath, userJson, "utf-8");
-
-  res.status(202).json(updatedUser);
+  res.status(202).json({
+    data: updatedUser,
+  });
 };
 
 // Deleting User by Id
 exports.deleteUserById = async (req, res) => {
   const id = req.params.id;
 
-  const data = await fs.promises.readFile(filepath, "utf-8");
-  let users = JSON.parse(data);
+  await User.deleteOne({ _id: id });
 
-  users = users.filter((user) => user.id !== parseInt(id));
-
-  const userJson = JSON.stringify(users);
-  await fs.promises.writeFile(filepath, userJson, "utf-8");
-
-  res.status(200).json();
+  res.status(204).json({});
 };
